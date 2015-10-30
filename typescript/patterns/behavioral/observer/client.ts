255744a51926a00da1a1d6f54cedf369c02dc6d9
Phillip Friedrich <friedrich.phillip@gmail.com>
2015-10-30 20:34:15 +0100

    update observer pattern

--- typescript/patterns/behavioral/observer/client.ts
+++ typescript/patterns/behavioral/observer/client.ts
@@ -1,3 +1,18 @@
+/**
+ * Define a one-to-many dependency between objects so that when oneobject changes 
+ * state, all its dependents are notified and updatedautomatically
+ *
+ * Usecase:
+ * 		--> When an abstraction has two aspects, one dependent on the 
+ * 				other.Encapsulating these aspects in separate objects lets you vary 
+ * 				and reuse them independently
+ * 		--> When a change to one object requires changing others, and you don't know 
+ * 				how many objects need to be changed
+ * 		--> When an object should be able to notify other objects without 
+ * 				making assumptions about who these objects are. In other words, you don't want 
+ * 				these objects tightly coupled
+ */
+
 'use strict';
 
 import {Stock} from './observer';
