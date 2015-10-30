15a8169d2d942a50f3f5135ba452e3da26db50c9
Phillip Friedrich <friedrich.phillip@gmail.com>
2015-10-30 20:17:30 +0100

    update iterator pattern

--- typescript/patterns/behavioral/iterator/client.ts
+++ typescript/patterns/behavioral/iterator/client.ts
@@ -1,5 +1,17 @@
 ///<reference path="../../../typings/tsd.d.ts" />
 
+/**
+ * Provide a way to access the elements of an aggregate objectsequentially without 
+ * exposing its underlying representation
+ *
+ * Usecase:
+ * 		--> to access an aggregate object's contents without exposing its 
+ * 				internalrepresentation
+ * 		--> to support multiple traversals of aggregate objects
+ * 		--> to provide a uniform interface for traversing different 
+ * 				aggregatestructures (that is, to support polymorphic iteration)
+ */
+
 import {Iterator} from './iterator';
 import {WeirdDataStructure} from './iterator';
 
